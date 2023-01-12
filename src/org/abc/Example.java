f4d91620e144d194fb7a3119dc0d8cec4b6578d0
KaviNano <kavinano100@gmail.com>
2023-01-12 09:22:54 +0530

    add code done

--- src/org/abc/Example.java
+++ src/org/abc/Example.java
@@ -4,7 +4,9 @@ public class Example {
 
 	public static void main(String[] args) {
 
-		int a = 10;
+		int a = 10, b = 30, c = 20;
 		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
 	}
 }
